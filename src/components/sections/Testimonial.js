9f6c0c5e2249b6d23dc8d878b9b3e78947287de8
huima22 <huima22@gmail.com>
2023-04-27 13:30:06 +0800

    Update the testimonials

--- src/components/sections/Testimonial.js
+++ src/components/sections/Testimonial.js
@@ -56,58 +56,92 @@ const Testimonial = ({
       <div className="container">
         <div className={innerClasses}>
           <SectionHeader data={sectionHeader} className="center-content" />
-          <div className={tilesClasses}>
+          <div className={tilesClasses} style = {{marginTop: '-42px'}} >
 
-            <div className="tiles-item reveal-from-right" data-reveal-delay="200">
+            <div className="tiles-item-new reveal-from-right" data-reveal-delay="200">
               <div className="tiles-item-inner">
                 <div className="testimonial-item-content">
                   <p className="text-sm mb-0">
-                    Databox technology will allow anybody to be very powerful data analyst and to look at data in a total different way.  </p>
+                   Right from the initial design work, to creating the operational service dashboard(OSD) in mid-2020,
+                   through to the version of the weekly report we published today, team Databox has been a critical partner on this journey,
+                    OSD provides us with a snapshot of the CCIB businesses through a few key metrics. The Databox team are the inspiration for the visualization and have navigated the
+                    numerous systems to obtain automated feeds and they continue to engage with the CCIB teams to evolve the dashboards capabilities.
+                     </p>
                 </div>
                 <div className="testimonial-item-footer text-xs mt-32 mb-0 has-top-divider">
-                  <span className="testimonial-item-name text-color-high">David Fleet</span>
+                  <span className="testimonial-item-name text-color-high">Graeme Peake</span>
 
                 </div>
                        <span className="testimonial-item-link text-xs">
-                                    <a href="#0">Global Head, IMO</a>
+                                    <a href="#0">COO-Trade ASEAN</a>
                                   </span>
               </div>
             </div>
 
-            <div className="tiles-item reveal-from-bottom">
+            <div className="tiles-item-new reveal-from-bottom">
               <div className="tiles-item-inner">
                 <div className="testimonial-item-content">
                   <p className="text-sm mb-0">
-                    Databox is a great example of something that can be useful, insightful and informative as you engage with your customers. </p>
+                    Our experience has been nothing short of excellent. We have collaboratively built insightful dashboards to understand the CX themes, trends,
+                    volumes and turnaround times across products, geographies and client segments. Every time we have challenged the team, they have pushed their own
+                    boundaries to come back with solutions that work. </p>
                 </div>
                 <div className="testimonial-item-footer text-xs mt-32 mb-0 has-top-divider">
-                  <span className="testimonial-item-name text-color-high">Robert Snell</span>
+                  <span className="testimonial-item-name text-color-high">Divashree Bhasin</span>
 
 
                 </div>
                     <span className="testimonial-item-link text-xs">
-                                    <a href="#0">MD, Co-Head GB Americans</a>
-                                  </span>
+                                    <a href="#0">Director, Client Strategy & Engagement, CCIB BD</a> </span>
+
+
               </div>
             </div>
 
-            <div className="tiles-item reveal-from-left" data-reveal-delay="200">
+            <div className="tiles-item-new reveal-from-center" data-reveal-delay="200">
               <div className="tiles-item-inner">
                 <div className="testimonial-item-content">
                   <p className="text-sm mb-0">
-                    Databox has become a single source of truth across the health of the lifecycle processes. So, it is critical tool in being able to manage the overdue rates </p>
+                    When I faced challenges to set up an interim process to aggregate COVID-19 related client credit extension. I reached out to the Databox team. The team was
+                     extremely proactive and quickly worked out a solution. Prior to this solution, the data as being maintained in Excel by each business team in the country, and then being manually collated.
+                     This was a tedious manual process and also exposed us to operational risk due to high risk of data errors. The entire end to end process was very well managed and was surely
+                     something that could not have been achieved without the team''s continued support.</p>
                 </div>
                 <div className="testimonial-item-footer text-xs mt-32 mb-0 has-top-divider">
-                  <span className="testimonial-item-name text-color-high">Warrick Sonn</span>
+                  <span className="testimonial-item-name text-color-high">Parikshit Yagnik</span>
 
 
                 </div>
 
                     <span className="testimonial-item-link text-xs ">
-                                    <a href="#0">MD, CDD and lifecycle management</a>
+                                    <a href="#0">Head, Process & Governance Credit</a>
                     </span>
               </div>
             </div>
+
+
+             <div className="tiles-item-new reveal-from-left" data-reveal-delay="200">
+                          <div className="tiles-item-inner">
+                            <div className="testimonial-item-content">
+                              <p className="text-sm mb-0">
+                               The client assessment and tail management(CATMAN) platform implemented on Databox provides several core capabilities
+                                regarding the analysis and identification of underperforming clients and the workflow management that supports the offboarding of those clients.
+                                The team does and outstanding job of supporting the business in these strategic initiatives: through the tools and capability made available but also
+                                in the way they partner and collaborate to drive value.
+                                </p>
+                            </div>
+                            <div className="testimonial-item-footer text-xs mt-32 mb-0 has-top-divider">
+                              <span className="testimonial-item-name text-color-high">Stephen Bastiaan Van Der Hor</span>
+
+
+                            </div>
+
+                                <span className="testimonial-item-link text-xs ">
+                                                <a href="#0">Head of Global Banking Operational Effectiveness CLDM</a>
+                                </span>
+                          </div>
+                        </div>
+
 
           </div>
         </div>
